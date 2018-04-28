8e377587f0ad8005f21c50445489fe18341b175d
kassandra <kassandrasmith@utexas.edu>
2018-04-28 01:22:47 -0500

    Can now read folders properly Add an attempt at comparing--not working. Add gitignore

--- src/assignment7/cheaters.java
+++ src/assignment7/cheaters.java
@@ -1,38 +1,83 @@
 package assignment7;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Hashtable;
-import java.util.Scanner;
+import java.util.*;
 
 public class cheaters {
+
+    //    Hashtable<String, Integer> phrases = new Hashtable<>();
     public static void main(String args[]) {
-        System.out.print(args);
-        File f = new File(args[0]);
-        int numWords = Integer.valueOf(args[1]); //todo - catch
-        Hashtable<String, Integer> phrases = new Hashtable<>();
+        ArrayList<Hashtable<String, Integer>> filesContents = new ArrayList<>();
+
+        //1) parse input
+
         try {
-            Scanner sc = new Scanner(f);
-            String[] words = sc.next().trim().replaceAll("[^a-zA-Z ]", " ").replaceAll("\\s+", " ").toLowerCase().split(" ");
-            for (int i = 0; i < words.length; i++) {
-                StringBuilder phrase = new StringBuilder();
-                for (int j = 0; j < numWords; j++) {
-                    phrase.append(words[i + j]).append(" ");
-                }
-                phrases.put(phrase.toString(), 0);
-            }
-
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            filesContents = parseInput(args[0], Integer.valueOf(args[1]));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.print("Incorrect number of inputs. Please try again using \"java cheaters [path to file] [number of words] [suspicion level]\"");
         }
+        //2) compare files
+        System.out.print("debug");
+        ArrayList<Integer>  debug  =  compareFiles(filesContents);
+        //3) print output
 
+                System.out.print("debug2"); // todo
+    }
 
+    private static ArrayList<Integer> compareFiles(ArrayList<Hashtable<String, Integer>> filesContents) {
+        ArrayList<Integer> collisions = new ArrayList(); // todo find some way of notating the two files that matched
+        for (int i = 0; i < filesContents.size(); i++) {
+            Hashtable first = filesContents.get(i);
+            for (int j = 0; j < filesContents.size(); j++) {
+                Hashtable second = filesContents.get(j);
+                int collided = 0;
+                if (first != second) { // same entry
+                    Set<String> firstKeys = first.keySet();
+                    Set<String> secondKeys = second.keySet();
+                    for (String s : firstKeys) {
+                        for (String st : secondKeys) {
+                            if (s.equals(st)) {
+                                collided++;
+                            }
+                        }
+                    }
+                    collisions.add(collided);
+                }
+            }
+        }return collisions;
+    }
+
+    private static ArrayList<Hashtable<String, Integer>> parseInput(String arg, Integer numWords) {
+        ArrayList<Hashtable<String, Integer>> filesContents = new ArrayList<>();
+
+        File folder = new File(arg);
+        for (File f : folder.listFiles()) {
+            Hashtable<String, Integer> phrases = new Hashtable<>();
+            try {
+                Scanner sc = new Scanner(new BufferedReader(new FileReader(f))); //todo
+
+                LinkedList<String> words = new LinkedList<>();
+
+                while (sc.hasNext()) {
+                    words.add(String.valueOf(sc.next()).replaceAll("[^a-zA-Z ]", "").toLowerCase()); //todo handle hyphens differently?
+                }
+
+                for (int i = 0; i < words.size() - numWords; i++) {
+                    StringBuilder phrase = new StringBuilder();
+                    for (int j = 0; j < numWords; j++) {
+                        phrase.append(words.get(j + i)).append(" ");
+                    }
+                    phrases.put(phrase.toString(), 0);
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            filesContents.add(phrases);
+        }
+        return filesContents;
     }
 }
